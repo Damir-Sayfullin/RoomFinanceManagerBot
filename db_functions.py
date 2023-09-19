4e6a6274606ebc8b26bc57873da7aad3cf6ecae0
Damir-Sayfullin <saifullin.d02@mail.ru>
2023-09-20 00:04:41 +0300

    create start menu

--- db_functions.py
+++ db_functions.py
@@ -45,3 +45,24 @@ def get_user_name(message):
     cur.close()
     conn.close()
     return query[0][0]
+
+
+def is_user_have_room(message):
+    """ Проверка наличия комнаты у пользователя в бд. Возвращает данные о комнате или False. """
+    conn = sqlite3.connect('chatbot.db')
+    cur = conn.cursor()
+    cur.execute('SELECT room_id FROM users WHERE id=?', (message.from_user.id,))
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+    if result[0][0]:
+        conn = sqlite3.connect('chatbot.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM rooms WHERE id=?', (result[0][0],))
+        result2 = cur.fetchall()
+        print(result2)
+        cur.close()
+        conn.close()
+        return result2
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import telebot
+from telebot import types
 import sqlite3
+
 from config import BOT_TOKEN
 import db_functions
 
@@ -29,7 +31,30 @@ def create_new_user(message):
 
 def start_menu(message):
     name = db_functions.get_user_name(message)
-    bot.send_message(message.chat.id, f"Добро пожаловать, <b>{name}</b>!", parse_mode='html')
+    room = db_functions.is_user_have_room(message)
+    if not room:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Создать новую комнату')
+        btn2 = types.KeyboardButton('Присоединиться к существующей')
+        markup.add(btn1, btn2)
+        bot.send_message(message.chat.id, f"Добро пожаловать, <b>{name}</b>!\nСейчас вы не состоите в комнате.",
+                         parse_mode='html', reply_markup=markup)
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Добавить покупку')
+        btn2 = types.KeyboardButton('Подтвердить покупку')
+        markup.row(btn1, btn2)
+        btn3 = types.KeyboardButton('Мои долги')
+        btn4 = types.KeyboardButton('Общие долги')
+        markup.row(btn3, btn4)
+        btn5 = types.KeyboardButton('График обязанностей')
+        btn6 = types.KeyboardButton('Информация о текущей комнате')
+        markup.add(btn5)
+        markup.add(btn6)
+        # bot.send_message(message.chat.id, f"Добро пожаловать, <b>{name}</b>!\nТекущая комната: {room[0][1]}",
+        #                  parse_mode='html', reply_markup=markup)
+        bot.send_message(message.chat.id, f"Добро пожаловать, <b>{name}</b>!\nТекущая комната: тест",
+                         parse_mode='html', reply_markup=markup)
 
 
 # тестовый обработчик
@@ -45,7 +70,6 @@ def test(message):
     for el in users:
         info += f'ID: {el[0]}, имя: {el[1]}, комната: {el[2]}\n'
     bot.send_message(message.chat.id, info)
-    print(message.from_user.id)
 
 
 bot.infinity_polling()
