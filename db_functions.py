5830ec92aa27280051d9ccd7a142bbb75ca94706
Damir-Sayfullin <saifullin.d02@mail.ru>
2023-09-19 23:19:44 +0300

    init git

---
+++ db_functions.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+def create_tables():
+    """ Создает таблицы в бд, если их не существует. """
+    conn = sqlite3.connect('chatbot.db')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS users ('
+                'id INTEGER PRIMARY KEY,'
+                'name VARCHAR(50) NOT NULL,'
+                'room_id VARCHAR(50))')
+    conn.commit()
+    # todo: другие таблицы
+    cur.close()
+    conn.close()
+
+
+def is_user_have_in_db(message):
+    """ Проверка наличия пользователя в бд. Возвращает True или False. """
+    conn = sqlite3.connect('chatbot.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users WHERE id=?', (message.from_user.id,))
+    query = cur.fetchall()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return False if not query else True
+
+
+def create_new_user(message):
+    """ Создание нового пользователя """
+    conn = sqlite3.connect('chatbot.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users (id, name) VALUES (?, ?)", (message.from_user.id, message.text))
+    conn.commit()
+    cur.close()
+    conn.close()
---
+++ main.py
@@ -0,0 +1,47 @@
+import telebot
+import sqlite3
+from config import BOT_TOKEN
+import db_functions
+
+# bot = telebot.TeleBot('6216891307:AAGzqwiMXr5TkTBJifKyuAd06z7l8_R0uCI')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    db_functions.create_tables()  # создание таблиц
+    if db_functions.is_user_have_in_db(message):
+        pass
+    else:
+        help_text = ''
+        with open("welcome.txt", "r", encoding='UTF8') as f:
+            for line in f.readlines():
+                help_text += line
+        bot.send_message(message.chat.id, help_text, parse_mode='html', disable_web_page_preview=True)
+        bot.send_message(message.chat.id, "<b>Введите ваше имя:</b>", parse_mode='html')
+        create_new_user(message)
+        bot.register_next_step_handler(message, create_new_user)
+
+
+def create_new_user(message):
+    db_functions.create_new_user(message)
+    bot.send_message(message.chat.id, f"Добро пожаловать, <b>{message.text}</b>!", parse_mode='html')
+
+
+# тестовый обработчик
+@bot.message_handler(commands=['test'])
+def test(message):
+    conn = sqlite3.connect('chatbot.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users")
+    users = cur.fetchall()
+    cur.close()
+    conn.close()
+    info = ''
+    for el in users:
+        info += f'ID: {el[0]}, имя: {el[1]}, комната: {el[2]}\n'
+    bot.send_message(message.chat.id, info)
+    print(message.from_user.id)
+
+
+bot.infinity_polling()
